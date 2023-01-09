9293a9e99955e753a3250e3a19af397290efb910
KapkavMori <sarlota.urbanova@gmail.com>
2023-01-09 20:33:27 +0100

    implementation Warrior class to main.py

--- main.py
+++ main.py
@@ -1,8 +1,9 @@
+from src.warrior import Warrior
 from src.fighter import Fighter
 from src.arena import Arena
 
 if __name__ == "__main__":
-	player_1 = Fighter("Amanda")
-	player_2 = Fighter("Bob")
+	player_1 = Warrior("Amanda")
+	player_2 = Warrior("Bob")
 	arena = Arena(player_1, player_2)
 	arena.fight(player_1, player_2)
