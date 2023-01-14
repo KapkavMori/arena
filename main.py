2e4f2ef6563a1772df03ddebc3665cd57ccaa414
KapkavMori <sarlota.urbanova@gmail.com>
2023-01-14 14:36:29 +0100

    změna odsazení z tab na 4 mezery

--- main.py
+++ main.py
@@ -2,7 +2,7 @@ from src.warrior import Warrior
 from src.arena import Arena
 
 if __name__ == "__main__":
-	player_1 = Warrior("Amanda")
-	player_2 = Warrior("Bob")
-	arena = Arena(player_1, player_2)
-	arena.fight(player_1, player_2)
+    player_1 = Warrior("Amanda")
+    player_2 = Warrior("Bob")
+    arena = Arena(player_1, player_2)
+    arena.fight(player_1, player_2)
